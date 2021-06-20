0f32be8ff1debf6c46f37eecde74d3e6e626e6c4
LorD_222 <ilya.r.200594@gmail.com>
2021-06-20 22:27:43 +0300

    add RDP

---
+++ RDP.bat
@@ -0,0 +1,3 @@
+netsh advfirewall set allprofiles state on
+netsh advfirewall firewall add rule name="RDP" dir=in action=allow protocol=TCP localport=3389 remote="192.168.0.1,192.168.0.97"
+rem netsh advfirewall firewall set rule group="remote desktop" new remoteip="192.168.0.1,192.168.0.97"
